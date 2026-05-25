dba6e2668134ee1f17c7acad09cf469ecee376c6
Ukhona20 <ST10520619@rcconnect.edu.za>
2026-05-25 16:15:29 +0200

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -4,23 +4,36 @@ public class Login {
     private String Password;
     private String firstName;
     private String lastName;
+    private boolean loggedIn=false;
+
 
     public Login(String UserName, String Password, String firstName, String lastName) {
-        UserName = UserName;
-        Password = Password;
-        firstName = firstName;
-        lastName = lastName;
+        this.UserName = UserName;
+        this.Password = Password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Login() {
+
     }
 
     public String loginUser(String enteredUserName, String enteredPassword) {
         if (enteredUserName.equals(this.UserName) && enteredPassword.equals(this.Password)) {
-            return "Welcome" + " " + firstName + " " + "It is great to see you again";
+            this.loggedIn=true;
+            QuickChat.startChat();
+            return "Welcome" + " " + firstName + " " + "It is great to see you again and BYE BYE!" ;
         } else {
             return "UserName or Password incorrect. Please try again.";
         }
     }
 
-
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+    public String getFirstName() { return firstName; }
+    public String getLastName()  {  return lastName; }
+    public String getUsername()  {  return UserName; }
 
 
 
